314ec6596d07e5b2ac3436fc81a6ab2b32301e9d
Adam Trepanier <atprogsoftware@gmail.com>
2017-02-12 11:34:36 -0600

    Remove quick checks from inital setup.

--- app/assets/javascripts/evidence.js
+++ app/assets/javascripts/evidence.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log("I was loaded");
     $('#evidence-filter').on("ajax:success", function (e, data, stat, xhr) {
         $('#evidence tbody').replaceWith(data);
     })
